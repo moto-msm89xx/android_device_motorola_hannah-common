838bdf2fba1e715f5e3e8269dd10d6b588181d16
Jarl-Penguin <jarlpenguin@outlook.com>
2020-12-15 11:37:04 +0300

    hannah-common: Initial bringup from hannah
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I2ee19d3f1efbfc8e8e0e4701598f438e3e6e8b11

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -14,4 +14,21 @@ export VENDOR=motorola
 
 export DEVICE_BRINGUP_YEAR=2019
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+source "./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+
+# Reinitialize the helper for device
+DEVICE_COMMON=hannah-common
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" true
+
+# Copyright headers and guards
+write_headers "ahannah hannah rhannah"
+
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" true
+
+# Finish
+write_footers
+
+# Fixup soong namespace
+sed -i "s|vendor/motorola/hannah-common|vendor/motorola/hannah|g" "${MY_DIR}/../../../vendor/${VENDOR}/${DEVICE}/${DEVICE}-vendor.mk"
