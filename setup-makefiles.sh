7fafff3506228eddca4f6d5e53a987c4573c7310
Bruno Martins <bgcngm@gmail.com>
2020-12-28 10:34:04 +0000

    hannah-common: Align with extraction-utils templates
    Change-Id: Ifbf18f79a1d0f173cb5b83bdf0178db2a5f1373e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,38 +1,14 @@
 #!/bin/bash
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
 
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+export BOARD_COMMON=msm8937-common
+export DEVICE_COMMON_GUARDS="ahannah hannah rhannah"
 
-LINEAGE_ROOT="${MY_DIR}/../../.."
-
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Initialize the helper for common
-INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
-
-# Copyright headers and guards
-write_headers "ahannah hannah rhannah"
-
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Finish
-write_footers
-
-export DEVICE_COMMON=msm8937-common
-
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR}/${BOARD_COMMON}/setup-makefiles.sh" "$@"
