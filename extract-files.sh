1674793e61e6cb36fa5a3655a3bd2c3466ed6047
Jarl-Penguin <jarlpenguin@outlook.com>
2021-05-14 09:58:23 +0300

    hannah-common: Decommonize camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I89a4125b653d584a549a0e600713913bcbaacd7b

--- extract-files.sh
+++ extract-files.sh
@@ -19,3 +19,10 @@ export DEVICE_COMMON=hannah-common
 export DEVICE_SPECIFIED_COMMON_DEVICE="ahannah hannah rhannah"
 
 "./../../${VENDOR}/${BOARD_COMMON}/extract-files.sh" "$@"
+
+DEVICE_BLOB_ROOT="../../../vendor/${VENDOR}/${DEVICE_COMMON}/proprietary"
+
+patchelf --set-soname camera.msm8937.so "${DEVICE_BLOB_ROOT}/vendor/lib/hw/camera.msm8937.so"
+patchelf --set-soname libactuator_dw9767_truly.so "${DEVICE_BLOB_ROOT}/vendor/lib/libactuator_dw9767_truly.so"
+sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_pproc_modules.so"
+sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${DEVICE_BLOB_ROOT}/vendor/lib/libmmcamera2_sensor_modules.so"
